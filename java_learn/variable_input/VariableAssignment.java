d9fbbdc5751c6ec3b3149a4f57900e1db02651a3
Yoshitaka <ystk.yoshitaka@gmail.com>
2025-05-31 19:02:07 +0900

    Add:variable_inputフォルダを追加。５つのファイルとREADMEを追加

---
+++ java_learn/variable_input/VariableAssignment.java
@@ -0,0 +1,18 @@
+// 変数の代入と、変数間の値のコピーを学ぶサンプル
+package java_learn.variable_input;
+
+public class VariableAssignment {
+    public static void main(String[] args)
+	{
+		int num1, num2;
+		
+		num1 = 3;
+		
+		System.out.println("変数num1の値は" + num1 + "です。");
+		
+		num2 = num1;
+		
+		System.out.println("変数num1の値をnum2に代入しました。");
+		System.out.println("変数num2の新しい値は" + num2 + "です。");
+	}
+}
